1a9cd4df284c7f839e98332dc0dccff3d96670ed
Aram <aramhrptn@hotmail.com>
2025-08-31 02:43:46 +0400

    Refactor AddFileRoutes to use handler functions for file operations

--- pkg/router/files.go
+++ pkg/router/files.go
@@ -2,15 +2,67 @@ package router
 
 import "github.com/gin-gonic/gin"
 
-func addFileRoutes(v1 *gin.RouterGroup) {
+// AddFileRoutes sets up the file endpoints.
+func AddFileRoutes(v1 *gin.RouterGroup) {
 	files := v1.Group("/file")
 
-	files.POST("/", func(c *gin.Context) {
-	})
-	files.GET("/:id", func(c *gin.Context) {
-	})
-	files.PATCH("/:id", func(c *gin.Context) {
-	})
-	files.DELETE("/:id", func(c *gin.Context) {
-	})
+	files.POST("/", CreateFileHandler)
+	files.GET("/:id", GetFileHandler)
+	files.PATCH("/:id", UpdateFileHandler)
+	files.DELETE("/:id", DeleteFileHandler)
+}
+
+// CreateFileHandler creates a new file
+// @Summary Create file
+// @Description Create a new file. Any user can perform this action.
+// @Tags files
+// @Accept json
+// @Produce json
+// @Param file body models.File true "File"
+// @Success 201 {object} models.File
+// @Failure 400 {object} router.ErrorResponse
+// @Router /v1/file/ [post]
+func CreateFileHandler(c *gin.Context) {
+	// Implementation goes here
+}
+
+// GetFileHandler gets a file by ID
+// @Summary Get file
+// @Description Get a file by ID. Any user can perform this action.
+// @Tags files
+// @Produce json
+// @Param id path string true "File ID"
+// @Success 200 {object} models.File
+// @Failure 404 {object} router.ErrorResponse
+// @Router /v1/file/{id} [get]
+func GetFileHandler(c *gin.Context) {
+	// Implementation goes here
+}
+
+// UpdateFileHandler updates a file by ID
+// @Summary Update file
+// @Description Update a file by ID. Any user can perform this action.
+// @Tags files
+// @Accept json
+// @Produce json
+// @Param id path string true "File ID"
+// @Param file body models.File true "File"
+// @Success 200 {object} models.File
+// @Failure 400 {object} router.ErrorResponse
+// @Failure 404 {object} router.ErrorResponse
+// @Router /v1/file/{id} [patch]
+func UpdateFileHandler(c *gin.Context) {
+	// Implementation goes here
+}
+
+// DeleteFileHandler deletes a file by ID
+// @Summary Delete file
+// @Description Delete a file by ID. Any user can perform this action.
+// @Tags files
+// @Param id path string true "File ID"
+// @Success 204 {object} nil
+// @Failure 404 {object} router.ErrorResponse
+// @Router /v1/file/{id} [delete]
+func DeleteFileHandler(c *gin.Context) {
+	// Implementation goes here
 }
