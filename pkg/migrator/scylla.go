92b5979bc813d9ecd6f577b8493b665d28698dbb
Aram <aramhrptn@hotmail.com>
2025-08-31 00:10:28 +0400

    wip

--- pkg/migrator/scylla.go
+++ pkg/migrator/scylla.go
@@ -1,16 +1,8 @@
 package migrator
 
-import "encoding/json"
-
 type scyllaMigration struct {
 	TableName string
 	Fields    map[string]string
 }
 
-func migrateScylla(model *scyllaMigration) {
-	bytes, err := json.Marshal(model)
-	if err != nil {
-		panic(err)
-	}
-	println(string(bytes))
-}
+func migrateScylla(model *scyllaMigration) {}
