fe5848c1f183b3e0f5ae5ad0d5493fd11f4a6564
urumo <aramhrptn@hotmail.com>
2025-09-21 00:00:47 +0400

    Refactor NewApplicationRepository to retrieve connection string from viper and improve error handling

--- pkg/repositories/main.go
+++ pkg/repositories/main.go
@@ -1,6 +1,10 @@
 package repositories
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 type ApplicationRepository struct {
 	db     any
@@ -11,9 +15,25 @@ type ApplicationRepository struct {
 	Files         IFileRepository
 }
 
-func NewApplicationRepository(dbType, connectionString string) (*ApplicationRepository, error) {
+func NewApplicationRepository(dbType string) (*ApplicationRepository, error) {
+	connectionString := viper.GetString(dbType)
+	if connectionString == "" {
+		return nil, fmt.Errorf("connection string for %s is not set", dbType)
+	}
 	switch dbType {
+	case "scylla":
+		return newScyllaRepository(connectionString)
+	case "postgres":
+		return newPostgresRepository(connectionString)
 	default:
 		return nil, fmt.Errorf("unsupported database type: %s", dbType)
 	}
+}
+
+func newPostgresRepository(connectionString string) (*ApplicationRepository, error) {
+	return nil, nil
+}
+
+func newScyllaRepository(connectionString string) (*ApplicationRepository, error) {
+	return nil, nil
 }
