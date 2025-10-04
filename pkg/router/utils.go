c47e8726d1cb6886f42f507a840f1c4b1d3687f6
eugene <evgenijantonenkov456@gmail.com>
2025-10-04 18:09:48 +0300

    add error code to 'writeError' func

--- pkg/router/utils.go
+++ pkg/router/utils.go
@@ -3,8 +3,8 @@ package router
 import "github.com/gin-gonic/gin"
 
 func writeError(c *gin.Context, code int, msg string) {
-	c.JSON(400, ErrorResponse{
-		Code:    400,
+	c.JSON(code, ErrorResponse{
+		Code:    code,
 		Message: msg,
 	})
 }
