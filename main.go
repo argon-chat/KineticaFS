257775538a1df33a6d87a07fa2028eb64c98a80c
urumo <aramhrptn@hotmail.com>
2025-10-06 05:10:54 +0400

    [skip ci] feat: add region configuration to init and update default values in config.yaml

--- main.go
+++ main.go
@@ -91,6 +91,7 @@ func init() {
 	viper.SetDefault("port", 3000)
 	viper.SetDefault("database", "scylla")
 	viper.SetDefault("front-end-path", "/var/www")
+	viper.SetDefault("region", "./regions.json")
 
 	pflag.BoolP("server", "s", false, "Run as server")
 	pflag.String("token", "", "Authorization token")
@@ -100,6 +101,7 @@ func init() {
 	pflag.StringP("database", "d", "scylla", "Database backend type (scylla, postgres)")
 	pflag.BoolP("bootstrap", "b", false, "Bootstrap admin service token (makes HTTP request to /v1/st/bootstrap)")
 	pflag.StringP("front-end-path", "f", "/var/www", "Path to front-end folder containing index.html (default: /var/www)")
+	pflag.StringP("region", "r", "./regions.json", "Path to regions configuration file (default: ./regions.json)")
 	pflag.Parse()
 	viper.BindPFlags(pflag.CommandLine)
 
