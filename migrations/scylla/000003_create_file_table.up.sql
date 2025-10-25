e94bf9336acd67ae599cb71e042fe4c333137c8a
urumo <aramhrptn@hotmail.com>
2025-10-25 08:54:26 +0400

    fix: reorder columns in File table creation script for consistency

--- migrations/scylla/000003_create_file_table.up.sql
+++ migrations/scylla/000003_create_file_table.up.sql
@@ -1,16 +1,17 @@
 -- Create File table
 CREATE TABLE IF NOT EXISTS File (
+    id text,
     file_size_limit varint,
     metadata text,
-    id text,
-    created_at timestamp,
-    updated_at timestamp,
     bucket_id text,
     file_size int,
     content_type text,
+    ref_count bigint,
     checksum text,
     name text,
     path text,
     finalized boolean,
+    created_at timestamp,
+    updated_at timestamp,
     PRIMARY KEY (id)
 );
